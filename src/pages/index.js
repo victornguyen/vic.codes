811d3fc5048997452aeed5556b04c098737c8fac
Victor Nguyen <vic.nguyen@gmail.com>
2019-05-30 09:04:34 +1000

    Refactor and remove unused cruft from Layout 🔥

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,11 @@ import { rhythm } from "../utils/typography"
 class BlogIndex extends React.Component {
   render() {
     const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
+    const author = data.site.siteMetadata.author
     const posts = data.allMarkdownRemark.edges
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
+      <Layout location={this.props.location} author={author}>
         <SEO title="All posts" />
         <Bio />
         {posts.map(({ node }) => {
@@ -50,6 +50,7 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+        author
       }
     }
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
