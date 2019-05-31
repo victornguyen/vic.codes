92d296a18aee71c7defb92af7df8bc6723ba93de
Victor Nguyen <vic.nguyen@gmail.com>
2019-05-31 10:04:11 +1000

    Implement styled-components in Bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -6,63 +6,69 @@
  */
 
 import React from 'react'
+import styled from 'styled-components'
 import { StaticQuery, graphql } from 'gatsby'
 import Image from 'gatsby-image'
 
-function Bio() {
-  return (
-    <StaticQuery
-      query={bioQuery}
-      render={data => {
-        const { author, social } = data.site.siteMetadata
-        return (
-          <div
-            style={{
-              display: `flex`,
-            }}
-          >
-            <Image
-              fixed={data.avatar.childImageSharp.fixed}
-              alt={author}
-              style={{
-                marginBottom: 0,
-                minWidth: 50,
-                borderRadius: `100%`,
-              }}
-              imgStyle={{
-                borderRadius: `50%`,
-              }}
-            />
-            <p>
-              <strong>{author}</strong> is a frontend developer living and
-              working in Melbourne who really likes{' '}
-              <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
-                JavaScript
-              </a>
-              , <a href="https://reactjs.org/">React</a> and{' '}
-              <a href="https://www.monsterhunterworld.com/" title="doot doot!">
-                video
-              </a>{' '}
-              <a
-                href="https://www.sekirothegame.com"
-                title="Hesitation is defeat"
-              >
-                games
-              </a>
-              .
-            </p>
-          </div>
-        )
-      }}
-    />
-  )
-}
+const Container = styled.header`
+  display: flex;
+`
 
-const bioQuery = graphql`
+const Avatar = styled(Image)`
+  margin: 0 20px 0 0;
+  min-width: 80px;
+  border-radius: 100%;
+`
+
+const Copy = styled.p`
+  font-size: 20px;
+`
+
+// TODO: import the copy (and links) for this from somewhere pls
+
+const Bio = () => (
+  <StaticQuery
+    query={query}
+    render={data => {
+      const { author, social } = data.site.siteMetadata
+      return (
+        <Container>
+          <Avatar
+            fixed={data.avatar.childImageSharp.fixed}
+            alt={author}
+            imgStyle={{
+              borderRadius: `50%`,
+            }}
+          />
+          <Copy>
+            <strong>{author}</strong> is a frontend developer living and working
+            in Melbourne who really likes{' '}
+            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
+              JavaScript
+            </a>
+            , <a href="https://reactjs.org/">React</a> and{' '}
+            <a href="https://www.monsterhunterworld.com/" title="doot doot!">
+              video
+            </a>{' '}
+            <a
+              href="https://www.sekirothegame.com"
+              title="Hesitation is defeat"
+            >
+              games
+            </a>
+            .
+          </Copy>
+        </Container>
+      )
+    }}
+  />
+)
+
+const query = graphql`
   query BioQuery {
     avatar: file(absolutePath: { regex: "/face.jpg/" }) {
       childImageSharp {
-        fixed(width: 50, height: 50) {
+        fixed(width: 80, height: 80) {
           ...GatsbyImageSharpFixed
         }
       }
