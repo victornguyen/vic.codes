1adbfc465c1978361fea7a326e6ca4b67a53a732
Victor Nguyen <vic.nguyen@gmail.com>
2019-06-04 22:32:53 +1000

    Align footer to the bottom of the viewport

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,10 +4,20 @@ import styled from 'styled-components'
 import { useSiteMetadata } from '../hooks'
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 60%;
+  height: 100vh;
   max-width: 780px;
   margin-left: auto;
   margin-right: auto;
+`
+
+const Footer = styled.footer`
+  margin-top: auto;
+  padding: 20px 0;
+  font-size: 14px;
+  color: var(--text-light);
 `
 
 const Layout = ({ children }) => {
@@ -15,11 +25,16 @@ const Layout = ({ children }) => {
   return (
     <Container>
       <main>{children}</main>
-      <footer>
+      <Footer>
         © {new Date().getFullYear()} {author}. Built with
         {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
+        <a href="https://www.gatsbyjs.org">Gatsby</a> and hosted on
+        {` `}
+        <a href="https://www.netlify.com">Netlify</a>. Check out this
+        site&rsquo;s
+        {` `}
+        <a href="https://github.com/victornguyen/vic.codes">source code</a>.
+      </Footer>
     </Container>
   )
 }
