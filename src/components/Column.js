97d3fe1edf0ec270043ea0c3db9fe3490278f883
Victor Nguyen <vic.nguyen@gmail.com>
2019-06-17 10:24:47 +1000

    Make Column node type configurable

--- src/components/Column.js
+++ src/components/Column.js
@@ -3,25 +3,31 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import sizes from '../styles/sizes'
 
-const Container = styled.section`
-  width: 85%;
-  margin-left: auto;
-  margin-right: auto;
+const createNode = type =>
+  styled[type]`
+    width: 85%;
+    margin-left: auto;
+    margin-right: auto;
 
-  @media (min-width: ${sizes.viewport7}) {
-    width: calc(65% + 1vh);
-    max-width: 780px;
-  }
-`
+    @media (min-width: ${sizes.viewport7}) {
+      width: calc(65% + 1vh);
+      max-width: 780px;
+    }
+  `
 
-// TODO: prop to customise node type?
-const Column = ({ children, className }) => (
-  <Container className={className}>{children}</Container>
-)
+const Column = ({ children, className, type }) => {
+  const ColumnNode = createNode(type)
+  return <ColumnNode className={className}>{children}</ColumnNode>
+}
 
 Column.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  type: PropTypes.oneOf(['div', 'header', 'footer', 'section', 'article']),
+}
+
+Column.defaultProps = {
+  type: 'div',
 }
 
 export default Column
