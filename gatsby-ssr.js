7d7b1150bdb7e7c69890a620d8c459f043d52eea
Victor Nguyen <vic.nguyen@gmail.com>
2020-07-28 21:23:45 +1000

    Inject style tag with color vars for non-js clients

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { COLORS } from './src/styles/colors'
 
+// <script> to inject to parse color mode from localStorage and set an initial
+// color mode and values for React to consume.
 const MagicScriptTag = () => {
   const codeToRunOnClient = `
 (function() {
@@ -38,8 +40,21 @@ const MagicScriptTag = () => {
   return <script dangerouslySetInnerHTML={{ __html: codeToRunOnClient }} />
 }
 
+// <style> to inject with color vars to serve to non-JS clients.
+const FallbackStyles = () => {
+  const cssVariableString = Object.entries(COLORS).reduce(
+    (string, [name, color]) => `${string}\n--color-${name}: ${color.light};`,
+    ''
+  )
+
+  const wrappedInSelector = `html { ${cssVariableString} }`
+
+  return <style>{wrappedInSelector}</style>
+}
+
 // A Gatsby SSR lifecycle method that lets us inject content onto pages at
 // compile time. https://www.gatsbyjs.org/docs/ssr-apis/#onRenderBody
-export const onRenderBody = ({ setPreBodyComponents }) => {
+export const onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
+  setHeadComponents(<FallbackStyles />)
   setPreBodyComponents(<MagicScriptTag />)
 }
