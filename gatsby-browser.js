60f713f0d508f0588a24b9fe26ef650defef08aa
Victor Nguyen <vic.nguyen@gmail.com>
2020-07-29 21:43:38 +1000

    Replace global.css with GlobalStyles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,6 @@
 import React from 'react'
 import App from './src/components/App'
 
-// typefaces
-import 'typeface-rubik'
-
-// css reset
-import 'sanitize.css'
-
-// global styles
-// TODO: replace with createGlobalStyles() from styled-components
-import './src/styles/global.css'
-
 // eslint-disable-next-line react/prop-types
 export const wrapRootElement = ({ element }) => {
   return <App>{element}</App>
--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { ThemeProvider } from './ThemeContext'
+import GlobalStyles from './GlobalStyles'
 
-const App = ({ children }) => <ThemeProvider>{children}</ThemeProvider>
+const App = ({ children }) => (
+  <ThemeProvider>
+    <GlobalStyles />
+    {children}
+  </ThemeProvider>
+)
 
 App.propTypes = {
   children: PropTypes.node,
