6d78a524497b1d7ea13f5630532614d449096bb6
Victor Nguyen <vic.nguyen@gmail.com>
2020-08-06 22:52:08 +1000

    Prevent creating styled components inside render

--- src/components/Column.js
+++ src/components/Column.js
@@ -3,27 +3,33 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import sizes from '../styles/sizes'
 
+const NODE_TYPES = ['div', 'header', 'footer', 'section', 'article']
+
 const createNode = (type) =>
   styled[type]`
     width: 85%;
     margin-left: auto;
     margin-right: auto;
-
     @media (min-width: ${sizes.viewport7}) {
       width: calc(65% + 1vh);
       max-width: 780px;
     }
   `
 
+const nodes = NODE_TYPES.reduce(
+  (acc, type) => ({ ...acc, [type]: createNode(type) }),
+  {}
+)
+
 const Column = ({ children, className, type }) => {
-  const ColumnNode = createNode(type)
+  const ColumnNode = nodes[type]
   return <ColumnNode className={className}>{children}</ColumnNode>
 }
 
 Column.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
-  type: PropTypes.oneOf(['div', 'header', 'footer', 'section', 'article']),
+  type: PropTypes.oneOf(NODE_TYPES),
 }
 
 Column.defaultProps = {
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,20 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
+const SIZES = ['1', '2', '3', '4', '5', '6']
+
 const createNode = (size) =>
   styled[`h${size}`]`
     margin-top: 1.5em;
     margin-bottom: 0.6em;
   `
 
+const nodes = SIZES.reduce(
+  (acc, type) => ({ ...acc, [type]: createNode(type) }),
+  {}
+)
+
 const Title = ({ children, size, ...rest }) => {
-  const Element = createNode(size)
-  return <Element {...rest}>{children}</Element>
+  const TitleNode = nodes[size]
+  return <TitleNode {...rest}>{children}</TitleNode>
 }
 
 Title.propTypes = {
   children: PropTypes.node,
-  size: PropTypes.oneOf(['1', '2', '3', '4', '5', '6']),
+  size: PropTypes.oneOf(SIZES),
 }
 
 Title.defaultProps = {
